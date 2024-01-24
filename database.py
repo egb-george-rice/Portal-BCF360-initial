211fc101cf65d70f9bd52fe6bdc89efda58e8f88
egb-george-rice <135032271+egb-george-rice@users.noreply.github.com>
2024-01-24 02:51:53 +0000

    secrets code and database connection

--- database.py
+++ database.py
@@ -1,18 +1,19 @@
-import sqlalchemy
+import sqlalchemy, os
 from sqlalchemy import create_engine, text
-#db_connection_string = "mysql+pymysql://bh194304n4kk1cn1fvxt:pscale_pw_VoGIj8rjKvF3SCi8829bwfjWTXtnS0oG1O8zdI8sPdD@aws.connect.psdb.cloud/bcfprojects?charset=utf8mb4"
-#engine = create_engine(db_connection_string, connect_args={
-  #"ssl": {
-    #"ssl_ca": "/etc/ssl/cert.pem"}})
-#connection = engine.connect()
-#result = connection.execute("SELECT * FROM PROJECTS")
 
-#def load_projects_from_db():
-  #with engine.connect() as conn:
-    #result = conn.execute(text("select * from projects"))
-#for row in result:
-  #print(row)
-    # projects = []
-    # for row in result.all():
-      # projects.append(row)
-    # return projects
+my_secret = os.environ['DB_CONNECTION_STRING']
+
+
+engine = create_engine(my_secret, connect_args={
+    "ssl": {
+      "ssl_ca": "/etc/ssl/cert.pem"
+    }
+  })
+
+def load_projects_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from projects"))
+    projects = []
+    for row in result.all():
+      projects.append(row)
+    return projects
--- main.py
+++ main.py
@@ -1,52 +1,52 @@
 from flask import Flask, render_template, jsonify
-#from database import load_projects_from_db
+from database import load_projects_from_db
 
 app = Flask(__name__)
 
-PROJECTS = [
-  {
-    'project_id': 1,
-    'project_county': 'Kern',
-    'project_state': 'CA',
-    'project_acres': '40',
-    'project_description': 'This is Project 1',
-    'project_image': 'County website'
-  },
-  {
-    'project_id': 2,
-    'project_county': 'Cumberland',
-    'project_state': 'NC',
-    'project_description': 'This is Project 2',
-    'project_image': 'County website'
-  },
-  {
-    'project_id': 3,
-    'project_county': 'Franklin',
-    'project_state': 'NC',
-    'project_acres': '1000',
-    'project_description': 'This is Project 3',
-    'project_image': 'County website'
-  },
-  {
-    'project_id': 4,
-    'project_county': 'Scotland',
-    'project_state': 'NC',
-    'project_acres': '1500',
-    'project_description': 'This is Project 4',
-    'project_image': 'County website'
-  }
-]
+# PROJECTS = [
+#   {
+#     'project_id': 1,
+#     'project_county': 'Kern',
+#     'project_state': 'CA',
+#     'project_acres': '40',
+#     'project_description': 'This is Project 1',
+#     'project_image': 'County website'
+#   },
+#   {
+#     'project_id': 2,
+#     'project_county': 'Cumberland',
+#     'project_state': 'NC',
+#     'project_description': 'This is Project 2',
+#     'project_image': 'County website'
+#   },
+#   {
+#     'project_id': 3,
+#     'project_county': 'Franklin',
+#     'project_state': 'NC',
+#     'project_acres': '1000',
+#     'project_description': 'This is Project 3',
+#     'project_image': 'County website'
+#   },
+#   {
+#     'project_id': 4,
+#     'project_county': 'Scotland',
+#     'project_state': 'NC',
+#     'project_acres': '1500',
+#     'project_description': 'This is Project 4',
+#     'project_image': 'County website'
+#   }
+# ]
 
 @app.route("/")
 
 def hello_bcf():
-  #projects = load_projects_from_db()
-  return render_template("home.html", projects=PROJECTS, company='Bright Clean Future')
+  projects = load_projects_from_db()
+  return render_template("home.html", projects=projects, company='Bright Clean Future')
 
 @app.route("/api/projects")
 def list_projects():
-  #projects = load_projects_from_db()
-  return jsonify(PROJECTS)
+  projects = load_projects_from_db()
+  return jsonify(projects)
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
   
