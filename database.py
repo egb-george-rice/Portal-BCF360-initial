27dfa440658bfc4e9414007ea5cf40ab64ec81ff
egb-george-rice <135032271+egb-george-rice@users.noreply.github.com>
2024-01-26 15:47:18 +0000

    converted results from list to dict

--- database.py
+++ database.py
@@ -13,7 +13,9 @@ engine = create_engine(my_secret, connect_args={
 def load_projects_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from proj24"))
-    projects = []
-    for (row) in result.all():
+    columns = result.keys()  
+    result_dict = [dict(zip(columns, row)) for row in result.fetchall()]
+    projects = []  
+    for row in result_dict:
       projects.append(row)
     return projects
